470e5f2ccac06a0b61884bb206d86a7e2ca99622
m rizki <rizki1997mr@gmail.com>
2024-08-01 10:43:16 +0700

    create success appointment an error

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import PatientForm from "@/components/forms/PatientForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+import Image from "next/image";
+import Link from "next/link";
 
-const NewAppointment = () => {
-  return <div>NewAppointment</div>;
-};
+export default async function NewAppointment({
+  params: { userId },
+}: SearchParamProps) {
+  const patient = await getPatient(userId);
 
-export default NewAppointment;
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <AppointmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
+          <p className="copyright mt-10 py-12">
+            © 2024 CarePulse. All rights reserved.
+          </p>
+        </div>
+      </section>
+      <Image
+        src={"/assets/images/appointment-img.png"}
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
---
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -0,0 +1,61 @@
+import { Doctors } from "@/constants";
+import { getAppointment } from "@/lib/actions/appointment.actions";
+import Image from "next/image";
+import Link from "next/link";
+
+const Success = async ({
+  params: { userId },
+  searchParams,
+}: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || "";
+  const appointment = await getAppointment(appointmentId);
+
+  const doctor = Doctors.find(
+    (doc) => doc.name === appointment.primaryPhysician
+  );
+  return (
+    <div className="flex h-screen max-h-screen px-[5%]">
+      <div className="success-img">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            width={1000}
+            height={1000}
+            className="h-10 w-fit"
+          />
+        </Link>
+
+        <section className="flex flex-col items-center">
+          <Image
+            src="/assets/gifs/success.gif"
+            alt="success"
+            width={280}
+            height={300}
+          />
+
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            Your <span className="text-green-500">appointment request</span> has
+            been successfully Submitted!
+          </h2>
+          <p>we will be in touch shortly to confirm</p>
+        </section>
+
+        <section className="request-details">
+          <p>Requested appointment details:</p>
+          <div className="flex items-center gap-3">
+            <Image
+              src={doctor?.image!}
+              alt="doctor"
+              width={100}
+              height={100}
+              className="size-6"
+            />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
